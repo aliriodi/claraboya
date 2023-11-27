03308b1c1a213163da4d28c551c3566738a02338
aliriodi <aliriodi@gmail.com>
2023-11-27 17:27:35 -0300

    cambiando textos

--- src/mocks/home_mock.js
+++ src/mocks/home_mock.js
@@ -5,23 +5,23 @@ import homec4 from "../assets/claraboya/Escuela1v2.png"
 
 export const home_mock = [
     {
-        titulo: "Iluminacion Natural en Aulas",
-        texto: "La instalación de claraboyas  en aulas escolares  va a permitir obtener los niveles de iluminación adecuados dentro del aula , obteniendo como beneficios la eficiencia energética, el bienestar de los estudiantes y el impacto positivo en el aprendizaje.",
+        titulo: "Potenciando la Educación con Luz Natural y Claraboyas Eficientes",
+        texto: "Proponemos el uso exclusivo de luz natural en las aulas de clases siempre que sea posible. Nuestra innovadora claraboya, diseñada para captar la luz del día y fabricada con materiales que cumplen con los requisitos lumino-térmicos, permite lograr impactos positivos en el entorno educativo.",
         img: homec2
     },
     {
-        titulo: "Mejorando Condiciones en Aulas",
-        texto: "Mejoramos las condiciones de confort visual y térmico en los edificios escolares mediante nuestros diseños, que dirijen y difunden uniformemente la luz del cielo, evitando a la vez la penetración de la radiación solar directa, que es el principal problema de los componentes cenitales cuando se les usa en zonas tropicales.",
+        titulo: "Mejorando las Condiciones de confort en el aula",
+        texto: "Optimizamos las condiciones de confort visual y térmico en los edificios escolares a través de nuestros diseños innovadores. Dirigimos y difundimos de manera uniforme la luz natural del cielo, al tiempo que prevenimos la penetración de la radiación solar directa. Este enfoque aborda de manera efectiva el principal desafío de los componentes cenitales, especialmente en zonas tropicales.",
         img: homec3
     },
     {
-        titulo: "Servicio de diseño gráfico e impresión",
-        texto: "Nos apasiona el diseño gráfico y contamos con experiencia en el manejo de diversos programas, lo que nos permite crear el diseño ideal de la papelería de tu evento.",
+        titulo: "Optimización Modular: Diseño Eficiente y Validación a través de Simulaciones",
+        texto: "Diseño modular de  (0,60 x 1,10 m) y  peso aproximado de 25 kg. Esta modulación se adapta a diferentes tipos de cubiertas livianas gracias a su versatilidad.  La ligereza y la capacidad autoportante facilitan la manipulación durante la fabricación y el montaje; simplicidad de las uniones y compatible con diferentes sistemas de fijación. Se llevaron a cabo comprobaciones y simulaciones utilizando software especializado con el objetivo de comparar las condiciones antes y después de la implementación. Estas simulaciones respaldaron la magnitud de las mediciones in situ, tanto en aspectos  térmicos como lumínicos. Confirmando que la claraboya cumple con los requerimientos de confort.",
         img: homec1
     },
     {
-        titulo: "Sitios aplicables",
-        texto: "Este enfoque es aplicable en escuelas con techos a dos aguas, y busca abordar el impacto ambiental y energético causado por el consumo eléctrico asociado con la iluminación artificial en las aulas. Según estimaciones, el consumo en una sola aula supera los 1430 kWh al año. Esta problemática afecta a más de 5500 centros escolares de educación primaria en Venezuela, según datos del INE (2012-2013).",
+        titulo: "Aplicación en las escuelas más desfavorecidos",
+       texto: "Esta solución  se adapta  principalmente a escuelas rurales y tiene como objetivo abordar los impactos ambientales y energéticos derivados del consumo eléctrico vinculado a la iluminación artificial en las aulas. Según  estudios el consumo en una sola aula supera los 1430 kWh anuales y hay alrededor de 5500 centros  escolares de educación primaria  (Venezuela INE 2012-2013)",
         img: homec4
     }
 ]
